038d68dc91ede5596402eebb7cadff02683f5bb4
Monir Ahmed <monir.ahmed0993@gmail.com>
2020-09-15 16:15:09 +0600

    const nums = [1, 2, 3, 4];

---
+++ NextGenJS/6.destructuring/destructuring.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4];
+[num1, num2, , num4] = numbers;
+
+console.log(num1, num2, num4);
+
+const person = {
+  name: 'Monir Ahmed',
+  age: 28
+}
+/** {name, age} = person;
+
+console.log(name, age); */
---
+++ NextGenJS/7.premitive-types-and-references/premitive-types-and-references.js
@@ -0,0 +1,30 @@
+/** Premitive Types */
+let number = 1;
+let newNumber = number;
+number = 2;
+console.log(newNumber);
+
+/** References */
+const person = {
+  name: "max"
+}
+
+const newPerson = person;
+person.name = "Menu";
+console.log(newPerson);
+
+/** If I want to actually copy the object to another object instead of assigning the reference, then 'spread' can be used */
+const personCopy = {
+  ...person
+}
+person.name = "Maisie"
+console.log(personCopy);
+console.log(newPerson);
+
+const nums = [1, 2, 3, 4];
+const doubleNums = nums.map((num) => {
+  return num * 2;
+});
+
+console.log(nums);
+console.log(doubleNums);
