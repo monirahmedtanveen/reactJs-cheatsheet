fa35ad966437959beaa5db3cde880d59ab258dc0
Monir Ahmed <monir.ahmed0993@gmail.com>
2020-09-21 16:20:58 +0600

    working with props

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <h1>Hi I'm a React App</h1>
       <p>This is really working</p>
-      <Person></Person>
-      <Person />
+      <Person name="Monir Ahmed" age="28"></Person>
+      <Person name="Kumu2.ewu" age="28"></Person>
+      <Person name="Alamin" age="32">My Hobbies: talking bullshit without any reason</Person>
     </div>
   );
 
